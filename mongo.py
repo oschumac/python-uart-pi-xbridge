7aae3f244f15d31e4d5a0c844df5cbbb5a594e84
Oliver Schumacher <olli@schumis-net.de>
2016-01-09 20:16:10 +0100

    update mongostring

--- mongo.py
+++ mongo.py
@@ -4,7 +4,6 @@ import pymongo
 from bson import BSON
 from bson import json_util
 
-MONGODB_URI_REMOTE = 'mongodb://Lars_2009:Lars65535@euve76271.serverprofi24.de:21060/larscgmtest' 
 MONGODB_URI_LOCAL = 'mongodb://aps:aps@127.0.0.1:27017/aps' 
 
 def getlast3():
